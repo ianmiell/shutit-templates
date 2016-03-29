7ed2f549c6f88a620d712652c36d08787777ecb1
ianmiell <ian.miell@gmail.com>
2016-03-29 15:29:52 +0100

    build template

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ then
     exit 1                                                                                                                                                           
 fi                                                                                                                                                                   
 pushd ..                                                                                                                                                             
-$SHUTIT build -d SHUTIT_TEMPLATE_skel_delivery "$@"                                                                                                                      
+$SHUTIT build -d {{ build.skel_delivery }} "$@"                                                                                                                      
 if [[ $? != 0 ]]                                                                                                                                                     
 then                                                                                                                                                                 
     popd                                                                                                                                                             
