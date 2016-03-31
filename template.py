d75497aa46b1f9ee8c5473e0d4b022166aaea07c
Ian Miell <ian.miell@gmail.com>
2016-03-31 10:43:14 +0100

    random

--- template.py
+++ template.py
@@ -71,11 +71,8 @@ class {{ skeleton.module_name }}(ShutItModule):
 		#                                    - Get input from user and return output
 		# shutit.fail(msg)                   - Fail the program and exit with status 1
 		#
-		vagrant_image = shutit.cfg[self.module_id]['vagrant_image'] + ''.join(random.choice(chars) for _ in range(6))
-		vagrant_provider = shutit.cfg[self.module_id]['vagrant_provider']
-		module_name = '{{ skeleton.module_name }}'
+		module_name = '{{ skeleton.module_name }}' + ''.join(random.choice(chars) for _ in range(6))
 		shutit.send('rm -rf /tmp/' + module_name + ' && mkdir -p /tmp/' + module_name + ' && cd /tmp/' + module_name)
-		shutit.send('vagrant init ' + vagrant_image)
 		shutit.send('vagrant up --provider virtualbox',timeout=99999)
 		shutit.login(command='vagrant ssh')
 		shutit.login(command='sudo su -',password='vagrant')
