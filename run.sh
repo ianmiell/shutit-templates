851dc617fa4824aa6b74679b71d0999a206df9ba
ianmiell <ian.miell@gmail.com>
2016-04-01 09:03:55 +0100

    modules

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ then
 	echo "Must have shutit on path, eg export PATH=$PATH:/path/to/shutit_dir"
 	exit 1
 fi
-$SHUTIT build -d bash -m shutit-library "$@"
+$SHUTIT build -d bash -m shutit-library/vagrant -m shutit-library/virtualbox "$@"
 if [[ $? != 0 ]]
 then
 	exit 1
