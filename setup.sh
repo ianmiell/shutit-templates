6b057ff0d10ae77ca7f694bb93d7c28eb3965e6e
ianmiell <ian.miell@gmail.com>
2016-04-04 15:32:32 +0100

    latest

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 chmod +x run.sh
 chmod 0400 configs/build.cnf
 mv template.py {{ skeleton.module_name }}.py
-git clone https://github.com/ianmiell/shutit-library
-rm -rf shutit-library/.git
+git init
+git submodule init
+git submodule add https://github.com/ianmiell/shutit-library
 
 cat << EOF
 ================================================================================
