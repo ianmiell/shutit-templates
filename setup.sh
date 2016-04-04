b4b5b0955561abbef2e4c0b9e967528704857ce5
ianmiell <ian.miell@gmail.com>
2016-04-04 15:32:32 +0100

    latest

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 chmod +x run.sh
 chmod 0400 configs/build.cnf
 mv template.py {{ skeleton.module_name }}.py
+git init
 git submodule init
 git submodule add https://github.com/ianmiell/shutit-library
 
