f91c03db6f835ca30c6ecad482632808e2d6e680
ianmiell <ian.miell@gmail.com>
2016-04-06 08:25:58 +0100

    forcepush

--- destroy_vms.sh
+++ destroy_vms.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 VBoxManage list runningvms | grep {{ skeleton.module_name }} | awk '{print $1}' | xargs --no-run-if-empty -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | awk '{print $1}'  | xargs --no-run-if-empty -IXXX VBoxManage unregistervm 'XXX' --delete
+
