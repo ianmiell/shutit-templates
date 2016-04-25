6999a90d822c805fa999c0ccec1d31027a5bc324
Ian Miell <ian.miell@gmail.com>
2016-04-25 18:25:41 +0100

    latest destroy

--- destroy_vms.sh
+++ destroy_vms.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-VBoxManage list runningvms | grep {{ skeleton.module_name }} | awk '{print $1}' | xargs --no-run-if-empty -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | awk '{print $1}'  | xargs --no-run-if-empty -IXXX VBoxManage unregistervm 'XXX' --delete
+while true 
+do
+	VBoxManage list runningvms | grep {{ skeleton.module_name }} | awk '{print $1}' | xargs --no-run-if-empty -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | awk '{print $1}'  | xargs --no-run-if-empty -IXXX VBoxManage unregistervm 'XXX' --delete
+	if [[ $(VBoxManage list vms | wc -l) == '0' ]]
+	then
+		break
+	else
+		ps -ef | grep virtualbox | grep {{ skelton.module_name }} | awk '{print $2}' | xargs kill
+		sleep 10
+	fi
+done
