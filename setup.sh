41e6721dd247b9e4fc774d442a81fd868fc78d5a
ianmiell <ian.miell@gmail.com>
2016-05-06 08:55:54 +0100

    msg

--- setup.sh
+++ setup.sh
@@ -13,6 +13,9 @@ Edit the Dockerfile to reflect the stages of the build.
 
 Edit the images.sh file to set the image name.
 
+Edit the configs/build.cnf to specify the relevant images that are allowed
+(eg '.*') and the starting one.
+
 Then edit the {{ skeleton.module_name }}.py file and add challenges etc.
 
 And then:
@@ -20,7 +23,7 @@ And then:
 	./images.sh # to build and push the image to dockerhub
 	./run.sh    # to run the tutorial
 
-An example is: https://github.com/ianmiell/git-bisect-tutorial
+An example is here: https://github.com/ianmiell/git-bisect-tutorial
 ================================================================================
 EOF
 
