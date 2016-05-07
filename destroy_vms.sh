0d7b9d1404963f1309e91878c39f359b21a8aab5
Ian Miell <ian.miell@gmail.com>
2016-05-07 21:56:00 +0100

    dvm

--- destroy_vms.sh
+++ destroy_vms.sh
@@ -2,7 +2,7 @@
 while true 
 do
 	VBoxManage list runningvms | grep {{ skeleton.module_name }} | awk '{print $1}' | xargs --no-run-if-empty -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | awk '{print $1}'  | xargs --no-run-if-empty -IXXX VBoxManage unregistervm 'XXX' --delete
-	if [[ $(VBoxManage list vms | wc -l) == '0' ]]
+	if [[ $(VBoxManage list vms | grep {{ skeleton.module_name }} | wc -l) == '0' ]]
 	then
 		break
 	else
