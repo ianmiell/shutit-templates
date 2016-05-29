ae9fce40920307de3e2ead53085507c33ace6885
Ian Miell <ian.miell@gmail.com>
2016-05-29 09:59:17 +0100

    options

--- template.py
+++ template.py
@@ -74,9 +74,25 @@ class {{ skeleton.module_name }}(ShutItModule):
 		#
 		vagrant_image = shutit.cfg[self.module_id]['vagrant_image']
 		vagrant_provider = shutit.cfg[self.module_id]['vagrant_provider']
+		gui = shutit.cfg[self.module_id]['gui']
+		memory = shutit.cfg[self.module_id]['memory']
 		module_name = '{{ skeleton.module_name }}_' + ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
 		shutit.send('rm -rf /tmp/' + module_name + ' && mkdir -p /tmp/' + module_name + ' && cd /tmp/' + module_name)
 		shutit.send('vagrant init ' + vagrant_image)
+		shutit.send_file('/tmp/' + module_name + '/Vagrantfile','''
+Vagrant.configure(2) do |config|
+  config.vm.box = "''' + vagrant_image + '''"
+  # config.vm.box_check_update = false
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  # config.vm.network "private_network", ip: "192.168.33.10"
+  # config.vm.network "public_network"
+  # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = ''' + gui + '''
+    vb.memory = "''' + memory + '''"
+    vb.name = "{{ skeleton.module_name }}"
+  end
+end''')
 		shutit.send('vagrant up --provider virtualbox',timeout=99999)
 		shutit.login(command='vagrant ssh')
 		shutit.login(command='sudo su -',password='vagrant')
@@ -119,6 +135,8 @@ class {{ skeleton.module_name }}(ShutItModule):
 		# shutit.cfg[self.module_id]['myconfig']
 		shutit.get_config(self.module_id,'vagrant_image',default='ubuntu/trusty64')
 		shutit.get_config(self.module_id,'vagrant_provider',default='virtualbox')
+		shutit.get_config(self.module_id,'gui',default='true')
+		shutit.get_config(self.module_id,'memory',default='1024')
 		return True
 
 def module():
