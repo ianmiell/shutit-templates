6501c8ece0735652d6bf29a0d387b20014f485b6
Ian Miell <ian.miell@gmail.com>
2016-06-05 21:34:35 +0100

    initial

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+MODULE_NAME="{{ skeleton.module_name }}"
+DIR="{{ skeleton.skel_path }}/$MODULE_NAME"
+DOMAIN="{{ skeleton.skel_domain }}"
+DELIVERY="{{ skeleton.skel_delivery }}"
+
+rm -rf $DIR
+shutit skeleton \
+	--shutitfile Shutitfile1 \
+	--shutitfile Shutitfile2 \
+	--module_directory ${DIR} \
+	--module_name ${MODULE_NAME} \
+	--domain ${DOMAIN} \
+	--delivery ${DELIVERY} \
+	--template_branch docker
+
+cd $DIR && ./run.sh
