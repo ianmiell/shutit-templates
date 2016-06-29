6e340192b90415147643f599b4c8bce4cbdc417d
Ian Miell <ian.miell@gmail.com>
2016-06-29 20:38:48 +0100

    comments

--- setup.sh
+++ setup.sh
@@ -8,22 +8,20 @@ mv template.py {{ skeleton.module_name }}.py
 
 
 cat << EOF
-================================================================================
-Edit the Dockerfile to reflect the stages of the build.
-
-Edit the images.sh file to set the image name.
-
-Edit the configs/build.cnf to specify the relevant images that are allowed
-(eg '.*') and the starting one.
-
-Then edit the {{ skeleton.module_name }}.py file and add challenges etc.
-
-And then:
-
-	./images.sh # to build and push the image to dockerhub
-	./run.sh    # to run the tutorial
-
-An example is here: https://github.com/ianmiell/git-bisect-tutorial
-================================================================================
+# Edit the Dockerfile to reflect the stages of the build.
+# 
+# Edit the images.sh file to set the image name.
+# 
+# Edit the configs/build.cnf to specify the relevant images that are allowed
+# (eg '.*') and the starting one.
+# 
+# Then edit the {{ skeleton.module_name }}.py file and add challenges etc.
+# 
+# And then:
+# 
+# 	./images.sh # to build and push the image to dockerhub
+# 	./run.sh    # to run the tutorial
+# 
+# An example is here: https://github.com/ianmiell/git-bisect-tutorial
 EOF
 
