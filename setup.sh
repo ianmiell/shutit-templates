7e4a0a625405c5911b0dc7cb01a2e99fe6f8c710
Ian Miell <ian.miell@gmail.com>
2016-06-30 07:07:53 +0100

    README

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@ chmod +x run.sh
 chmod 0400 configs/build.cnf
 mv template.py {{ skeleton.module_name }}.py
 
-cat << EOF
+cat > README.md << EOF
 # Run:
 cd $(pwd) && ./run.sh
 # to run.
 EOF
 
+cat README.md
